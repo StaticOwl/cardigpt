5968fd3b3976715dcb01ec6204944740d2be8b17
StaticOwl <akostuv@gmail.com>
2024-10-06 19:40:02 -0400

    Updating Predictive model code with latest changes

--- pred_res/main.py
+++ pred_res/main.py
@@ -10,9 +10,9 @@ from logging_config import setup_logging
 
 import logging
 
-from .train.train import Trainer
-from .utils.datasplit import read_and_split_data
-from .utils.downloader import kaggle_data_downloader
+from train.train import Trainer
+from utils.datasplit import read_and_split_data
+from utils.downloader import kaggle_data_downloader
 
 logger = logging.getLogger(__name__)
 
