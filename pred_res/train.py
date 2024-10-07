6a4a43785a61656458da853d026336a8642636b6
StaticOwl <akostuv@gmail.com>
2024-10-07 14:55:33 -0400

    creating save_dir if not exists

--- pred_res/train.py
+++ pred_res/train.py
@@ -8,6 +8,7 @@ Description: write_a_description
 import logging
 import math
 import os
+import shutil
 import time
 
 import torch
@@ -91,6 +92,19 @@ class Trainer:
         self.criterion = nn.CrossEntropyLoss()
         self.lr_scheduler = optim.lr_scheduler.StepLR(self.optimizer, step_size=args.step_size, gamma=args.gamma)
         self.model.to(self.device)
+
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+        else:
+            for f in os.listdir(self.save_dir):
+                file_path = os.path.join(self.save_dir, f)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    logging.error(e)
 
     def train(self):
         """
