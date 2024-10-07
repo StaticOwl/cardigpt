8f4891481b295e8d27761a551c0418e65f663f44
StaticOwl <akostuv@gmail.com>
2024-10-06 22:52:33 -0400

    modifying main running script

--- pred_res/utils/downloader.py
+++ pred_res/utils/downloader.py
@@ -3,12 +3,14 @@ File: downloader.py
 Project: potluck
 Author: malli
 Created: 06-10-2024
-Description: write_a_description
+Description: Download data from Kaggle and split it into train and test sets
 """
+
 import json
 import logging
 import os
 import shutil
+from pathlib import Path
 
 from kaggle import KaggleApi
 from sklearn.model_selection import train_test_split
@@ -18,6 +20,16 @@ logger = logging.getLogger(__name__)
 
 
 def kaggle_data_downloader(datadict, flag=False):
+    """
+    Download data from Kaggle and split it into train and test sets.
+
+    Parameters
+    ----------
+    datadict : dict
+        Dictionary containing dataset information
+    flag : bool
+        Flag to indicate whether to download and split the data
+    """
     api = KaggleApi()
     api.authenticate()
 
@@ -25,34 +37,36 @@ def kaggle_data_downloader(datadict, flag=False):
         logger.info('Starting data download process')
 
         # Clear existing data directory
-        if os.path.exists('input_data'):
-            shutil.rmtree('input_data')
-        os.makedirs('input_data')
+        data_dir = Path('input_data')
+        if data_dir.exists():
+            logger.info('Removing existing data directory')
+            shutil.rmtree(data_dir)
+        data_dir.mkdir(parents=True, exist_ok=True)
 
         # Create train and test directories globally
-        train_dir = os.path.join('input_data', 'train')
-        test_dir = os.path.join('input_data', 'test')
-        os.makedirs(train_dir, exist_ok=True)
-        os.makedirs(test_dir, exist_ok=True)
+        train_dir = data_dir / 'train'
+        test_dir = data_dir / 'test'
+        train_dir.mkdir(exist_ok=True)
+        test_dir.mkdir(exist_ok=True)
 
         # Download and process each dataset
         for key, value in tqdm(datadict.items(), desc='Downloading data'):
-            dataset_path = 'input_data'
+            dataset_path = data_dir
 
-            if key not in os.listdir(dataset_path):
+            if key not in dataset_path.iterdir():
                 api.dataset_download_files(value['source'], path=dataset_path, unzip=True, quiet=False)
-                folder_path = os.path.join(dataset_path, value['inside_folder'])
+                folder_path = dataset_path / value['inside_folder']
 
                 # Move files from inside folder to the main dataset path
-                for filename in os.listdir(folder_path):
-                    os.rename(os.path.join(folder_path, filename), os.path.join(dataset_path, filename))
+                for file in folder_path.iterdir():
+                    file.rename(dataset_path / file.name)
                 shutil.rmtree(folder_path)
 
             # Split data into train and test sets
-            all_files = [f for f in os.listdir(dataset_path) if f not in ['train', 'test']]
+            all_files = [f for f in dataset_path.iterdir() if f.name not in ['train', 'test']]
             grouped_files = {}
             for file in all_files:
-                basename, _ = os.path.splitext(file)
+                basename, _ = os.path.splitext(file.name)
                 if basename not in grouped_files:
                     grouped_files[basename] = []
                 grouped_files[basename].append(file)
@@ -62,13 +76,14 @@ def kaggle_data_downloader(datadict, flag=False):
             # Move files to respective directories
             for group in train_group:
                 for file in group:
-                    shutil.move(os.path.join(dataset_path, file), os.path.join(train_dir, file))
+                    file.rename(train_dir / file.name)
             for group in test_group:
                 for file in group:
-                    shutil.move(os.path.join(dataset_path, file), os.path.join(test_dir, file))
+                    file.rename(test_dir / file.name)
 
         # Save updated datadict with paths (optional)
-        with open('datadict.json', 'w') as f:
-            json.dump(datadict, f)
+        if flag:
+            with open('datadict.json', 'w') as f:
+                json.dump(datadict, f)
 
-        logger.info('Finished downloading and splitting data')
+            logger.info('Finished downloading and splitting data')
