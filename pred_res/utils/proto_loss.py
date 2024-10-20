4a38e40d3255c53aef18f3f9475e19e53bc02399
StaticOwl <akostuv@gmail.com>
2024-10-19 20:27:17 -0400

    Updates ===================== - Updating Prototype Loss Function

--- pred_res/utils/proto_loss.py
+++ pred_res/utils/proto_loss.py
@@ -11,9 +11,9 @@ import torch.nn.functional as F
 
 def proto_loss(prototype, eps=1e-6):
     pairwise_distance = F.pdist(prototype, p=2)
-    if pairwise_distance.size(0) > 2:
+    if pairwise_distance.size(0) < 2:
         return torch.tensor(0.0, requires_grad=True).to(prototype.device)
-    min_dist, _ = torch.min(F.pdist(prototype, p=2).view(-1, 1), dim=0)
+    min_dist, _ = torch.min(pairwise_distance.view(-1, 1), dim=0)
     avg_min_dist = torch.mean(min_dist)
     loss = 1.0 / (torch.log(1.0 / avg_min_dist + eps) + eps)
     return loss
